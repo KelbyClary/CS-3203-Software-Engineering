e5f70983568430fae35dbe6237e3cb2fcbd6495d
KelbyClary <kelbyclary@DESKTOP-FG6DDQQ.localdomain>
2022-09-30 20:23:51 -0500

    Finish

--- express.js
+++ express.js
@@ -7,69 +7,84 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
 var fs = require('fs');
-
+var searchedArray = [];
 
 //global variable for tweet data
 var tweetinfo = []
 var newArray = []
 //load the input file
-fs.readFile('favs.json', 'utf8', function readFileCallback(err,data ){
-  if(err){
+fs.readFile('favs.json', 'utf8', function readFileCallback(err, data) {
+  if (err) {
     req.log.info('cannot load a file:' + fileFolder + '/' + _file_name)
     throw err;
   }
-  else{
+  else {
     tweetinfo = JSON.parse(data);
-    for(var index in tweetinfo) newArray.push({ "id": tweetinfo[index]["user"]["id"], "screenName": tweetinfo[index]["user"]["screen_name"], "name": tweetinfo[index]["user"]["name"], "text": tweetinfo[index]["text"], "created_at": tweetinfo[index]["created_at"]})
-    
-      }
+    //for loop that adds the data from JSON file
+    for (var index in tweetinfo) newArray.push({ "id": tweetinfo[index]["user"]["id"], "screenName": tweetinfo[index]["user"]["screen_name"], "name": tweetinfo[index]["user"]["name"], "text": tweetinfo[index]["text"], "created_at": tweetinfo[index]["created_at"] })
+  }
 });
- 
-
-
 //Get functions
 //Shows user info
-app.get('/tweets', function(req, res) {
-  //TODO: send all users' IDs
-  res.send({newArray: newArray})
+app.get('/tweets', function (req, res) {
+  res.send({ newArray: newArray })
 });
-
 //Shows tweet info
-app.get('/tweetinfo', function(req, res) {
-  //TODO: send tweet info
-  res.send({newArray: newArray})
+app.get('/tweetinfo', function (req, res) {
+  res.send({ newArray: newArray })
 });
-
 //Shows searched tweets
-app.get('/searchinfo', function(req, res){
-  //TODO: send searched tweets
-
-});
-
-//Post functions
+app.get('/searchinfo', function (req, res) {
+  res.send({searchedArray: searchedArray})
+})
 //Posts created tweets
-app.post('/tweetinfo', function(req, res) {
-  //TODO: create a tweet.
-
+app.post('/tweetinfo', function (req, res) {
+  var inputID = Number(req.body.id);
+  var inputText = req.body.text;
+  var todayDate = new Date();
+  //Variable to get the current time for the create tweets
+  var currentTime = todayDate.getHours() + ":" + todayDate.getMinutes() + ":" + todayDate.getSeconds();
+ //Addes in a new element to the newArray
+  newArray.push({
+    id: inputID,
+    name: 'Kelby',
+    screenName: 'KelbyClary',
+    text: inputText,
+    created_at: currentTime
+  })
 });
-
 //Posts searched tweets
-app.post('/searchinfo', function(req, res) {
-  //TODO: search a tweet
+app.post('/searchinfo', function (req, res) {
+  var inputID = req.body.inputID;
+  var searchArray = [];
+  //for loop that when the current index id matches the inputID to update
+  for (var index in newArray) {
+    if (newArray[index]['id'] == inputID) {
+      searchArray.push({ 'id': newArray[index]['id'], 'text': newArray[index]['text'], 'created_at': newArray[index]['created_at']});
+      searchedArray.push({ 'id': newArray[index]['id'], 'text': newArray[index]['text'], 'created_at': newArray[index]['created_at']});
+    } 
+  }  
+  res.send({searchArray:searchArray});
 });
-
-//Update
-app.put('/tweets/:nm', function(req, res) {
-  //TODO: update tweets
+app.put('/tweets/:nm', function (req, res) {
+  var input = req.params.nm;
+  var inputNewScreenName = req.body.inputNewScreenName;//Variable to hold the new input screen name
+  //For loop to handle when the user is looking for the new name 
+  for (var key in newArray)
+    if ((newArray[key]['name'] === input)) {
+     newArray[key]['screenName'] = inputNewScreenName 
+    };
 });
-
 //Delete 
-app.delete('/tweetinfo/:tweetid', function(req, res) {
-  //TODO: delete a tweet
-
+app.delete('/tweetinfo/:tweetid', function (req, res) {
+  var input = req.params.tweetid;
+  //For loop that when the index of the id is equal to input set as a number then splice the index at position 1
+  for (var index in newArray) {
+    if (newArray[index]['id'] == Number(input)) {
+      newArray.splice(index, 1)
+    }
+  }
 });
-
-
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log('Server listening on ' + PORT);
 });
--- scripts.js
+++ scripts.js
@@ -1,101 +1,138 @@
-
-$(function() {
-   //Get 
-   $('#get-button').on('click', function() {
-        //TODO: get all users' IDs & display it
-        $.ajax({
-          url: '/tweets',
-          contentType: 'application/json',
-          success: function(response){
-            var tbodyEl = $('#namebody');
-            tbodyEl.html('');
-            response.newArray.forEach(function(newArray) {
-              console.log(newArray)
-              tbodyEl.append('\
+$(function () {
+  //This function will get the tweets on button click
+  $('#get-button').on('click', function () {
+    $.ajax({
+      url: '/tweets',
+      contentType: 'application/json',
+      success: function (response) {
+        var tbodyEl = $('#namebody');
+        tbodyEl.html('');
+        response.newArray.forEach(function (newArray) {
+          tbodyEl.append('\
               <tr>\
               <td class="id">' + newArray.id + '</td>\
               <td><input type="text" class="name" value="' + newArray.screenName + '"></td>\
               <td><input type="text" class="name" value="' + newArray.name + '"></td>\
               </tr>\
               ');
-              });
-              }
-              });
-   });
-
-
-    //Get tweets
-    $('#get-tweets-button').on('click', function(){
-        //TODO: get tweet info and display it
-        $.ajax({
-          url: '/tweetinfo',
-          contentType: 'application/json',
-          success: function(response) {
-              var tbodyEl = $('#tweetbody');
-
-              tbodyEl.html('');
-
-              response.newArray.forEach(function(newArray) {
-                  tbodyEl.append('\
+        });
+      }
+    });
+  });
+  //Get tweets from the JSON data
+  $('#get-tweets-button').on('click', function () {
+    $.ajax({
+      url: '/tweetinfo',
+      contentType: 'application/json',
+      success: function (response) {
+        var tbodyEl = $('#tweetbody');
+        tbodyEl.html('');
+        response.newArray.forEach(function (newArray) {
+          tbodyEl.append('\
                       <tr>\
                           <td class="id">' + newArray.id + '</td>\
                           <td><input type="text" class="name" value="' + newArray.text + '"></td>\
                           <td><input type="text" class="name" value="' + newArray.created_at + '"></td>\
                       </tr>\
                   ');
-              });
-          }
-      });
+        });
+      }
     });
-
-    //Get searched tweets
-    $('#get-searched-tweets').on('click', function() {
-        //TODO: get a searched tweet(s) & display it
+  });
+  //Get recently searched tweets
+  $('#get-searched-tweets').on('click', function () {
+    $.ajax({
+      url: '/searchinfo',
+      contentType: 'application/json',
+      success: function (response) {
+        var tbodyEl = $('#searchbody');
+        tbodyEl.html('');
+        response.searchedArray.forEach(function (searchedArray) {
+          //Bringing in the id,text,created_at data
+          tbodyEl.append('\
+                      <tr>\
+                          <td class="id">' + searchedArray.id + '</td>\
+                          <td><input type="text" class="name" value="' + searchedArray.text + '"></td>\
+                          <td><input type="text" class="name" value="' + searchedArray.created_at + '"></td>\
+                      </tr>\
+                  ');
+        });
+      }
     });
-
-
+  });
   //CREATE
-  $('#create-form').on('submit', function(event){
-        event.preventDefault();
-
-        var createInput = $('#create-input');
-
-        //TODO: creat a tweet
-  });
-
-    //Create searched tweets
-  $('#search-form').on('submit', function(event){
+  $('#create-form').on('submit', function (event) {
     event.preventDefault();
-    var userID = $('#search-input');
-    
-    //TODO: search a tweet and display it.
-
+    var createInput = $('#create-input').val();//Creates input and gets the value
+    var s = createInput.split(';');//Used to split the input entries seperated by a ; character
+    $.ajax({
+      url: '/tweetinfo',
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ id: s[0], text: s[1] }),//Converting javscript value to a JSON String
+      success: function (response) {
+        createInput.val('');
+      }
+    })
   });
-
+  //Create searched tweets
+  $('#search-form').on('submit', function (event) {
+    event.preventDefault();
+    var inputID = $('#search-input').val();
+    $.ajax({
+      url: '/searchinfo',
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ inputID: inputID }),//Converting the inputID to a JSON string
+      success: function (response) {
+        console.log(response)
+        var tbodyEl = $('#searchbody');
+        tbodyEl.html('');
+        response.searchArray.forEach(function (searchArray) {//Creating the data from the searched tweet
+          tbodyEl.append('\
+                      <tr>\
+                          <td class="id">' + searchArray.id + '</td>\
+                          <td><input type="text" class="name" value="' + searchArray.text + '"></td>\
+                          <td><input type="text" class="name" value="' + searchArray.created_at + '"></td>\
+                      </tr>\
+                  ');
+        }
+        )
+      }
+    })
+  });
   //UPDATE/PUT
-  $("#update-user").on('submit', function(event){
-      event.preventDefault();
-    var updateInput = $('#update-input');
-    var inputString = updateInput.val();
-
-    const parsedStrings = inputString.split(';');
-
-    var name = parsedStrings[0];
-    var newName = parsedStrings[1];
-    
-    //TODO: update a tweet
-
-  });
-
-
-  //DELETE
-  $("#delete-form").on('submit', function() {
-    var id = $('#delete-input')
+  $("#update-user").on('submit', function (event) {
     event.preventDefault();
-
-    //TODO: delete a tweet
-
+    var updateInput = $('#update-input');
+    var inputString = updateInput.val();//Updates the input value 
+    const parsedStrings = inputString.split(';');//Splits the input by the character ;
+    //This is used to seperate the incoming input to retrieve the name and the screenName
+    var inputname = parsedStrings[0];
+    var inputNewScreenName = parsedStrings[1];
+    $.ajax({
+      url: '/tweets/' + inputname,
+      method: 'PUT',
+      contentType: 'application/json',
+      data: JSON.stringify({ inputNewScreenName: inputNewScreenName }),//Converting the inputnewScreenName value into a JSON string
+      success: function (response) {
+        console.log(response);
+      }
+    });  
   });
-
-
-});
+  //DELETE
+  //Deletes the tweet when given the ID
+  $("#delete-form").on('submit', function () {
+    var id = $('#delete-input').val()
+    event.preventDefault();
+    $.ajax({
+      url: '/tweetinfo/' + id, 
+      method: 'DELETE',
+      contentType: 'application/json',
+      data: JSON.stringify({ input: id }),
+      success: function (response) {
+        console.log(response);
+      }
+    })
+  });
+})
